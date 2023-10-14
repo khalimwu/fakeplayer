df829b5c522c10b41ef55c288d8933a5c980630b
khalimwu <u201617896@upc.edu.pe>
2023-10-14 12:42:14 -0500

    cleaning up and fixing 1.20.2
    working state 1.20.2

--- nms/v1_20_R2/build.gradle.kts
+++ nms/v1_20_R2/build.gradle.kts
@@ -4,15 +4,14 @@ plugins {
 
 repositories {
   maven("https://repo.opencollab.dev/maven-releases")
+  maven("https://repo.opencollab.dev/maven-snapshots")
 }
 
 dependencies {
   paperDevBundle("1.20.2-R0.1-SNAPSHOT")
 
-  implementation(files("..\\..\\libs\\mcprotocollib-1.20-1.jar"))
-  implementation("org.cloudburstmc.math:immutable:2.0")
-  implementation("com.github.GeyserMC:opennbt:1.5")
-  implementation("com.github.GeyserMC:mcauthlib:6f3d6aada5")
+//  implementation(files("..\\..\\libs\\mcprotocollib-1.20-1.jar"))
+  implementation("com.github.steveice10:mcprotocollib:1.20.2-1-SNAPSHOT")
 
 //  implementation("com.github.steveice10:mcprotocollib:1.19.2-1") {
 //    exclude("net.kyori:adventure-text-serializer-gson")
